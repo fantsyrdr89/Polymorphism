67a0b163f6779c008869b72ca94dc73344e5f982
Alexandra Armstrong <alexandraarmstrong@zipcoders-MacBook-Pro.local>
2017-01-30 13:11:52 -0500

    Stubs for IO tests

---
+++ src/main/java/io/zipcoder/pets/HelloUser.java
@@ -0,0 +1,23 @@
+package io.zipcoder.pets;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class HelloUser {
+    static Scanner scanner = new Scanner(System.in);
+    static Scanner scanner1 = new Scanner(System.in);
+    static int numberOfPets;
+    static HashMap <String, String> pets = new HashMap<String, String>();
+    public static void main(String[] args){
+
+    }
+
+    public static void inputNumberOfPets(){
+
+    }
+
+    public static void inputPets(){
+
+    }
+}
